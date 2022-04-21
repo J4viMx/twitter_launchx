aeebc799d2cdbb50016f96635b630f3d1ec21c81
JaviMx <javimxmine@hotmail.com>
2022-04-21 18:19:47 -0500

    Requerimiento 2

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -3,14 +3,14 @@ const User = require('../../app/models/User')
 describe("Unit Test for User Class", () =>{
     test('Create an User object', () => {
 
-        const user = new User(1, "JaviMx", "Javier", "Bio", "dateCreated","lastUpdated")
+        const user = new User(1, "JaviMx", "Javier", "Bio")
 
         expect (user.id).toBe(1)
         expect (user.username).toBe("JaviMx")
         expect (user.name).toBe("Javier")
         expect (user.bio).toBe("Bio")
-        expect (user.dateCreated). toBe("dateCreated")
-        expect (user.lastUpdated).toBe("lastUpdated")
+        expect (user.dateCreated).not.toBeUndefined()
+        expect (user.lastUpdated).not.toBeUndefined()
         });
         
 });
