2d592058924e114a4dfb4d2e872911c4cad7e8c2
JaviMx <javimxmine@hotmail.com>
2022-04-21 18:29:03 -0500

    Requerimiento 4

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -20,5 +20,16 @@ describe("Unit Test for User Class", () =>{
         expect (user.getDateCreated).not.toBeUndefined ()
         expect (user.getLastUpdated).not.toBeUndefined()
     });
+
+    test('Add setters', () => {
+        const user = new User(1, "carlogilmar", "Carlo", "Bio")
+        user.setUsername = "Gilmar"
+        expect (user.username).toBe("Gilmar")
+        
+        user.setBio = "New bio"
+        expect (user.bio).toBe("New bio")
+    
+    });
+        
             
 });
