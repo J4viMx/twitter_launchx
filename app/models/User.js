d3d7f4c8daf71b2d31d984005dcb4b3fa3779e78
JaviMx <javimxmine@hotmail.com>
2022-04-21 18:24:49 -0500

    Requerimiento 3

--- app/models/User.js
+++ app/models/User.js
@@ -7,8 +7,19 @@ class User {
         this.bio = bio
         this.dateCreated = new Date()
         this.lastUpdated = new Date()
-
     }
+        get getUsername (){
+            return this.username
+        }
+        get getBio(){
+            return this.bio
+        }
+        get getDateCreated (){
+            return this.dateCreated
+        }
+        get getLastUpdated(){
+            return this.lastUpdated
+        } 
 }
 
 module.exports = User
--- test/models/User.test.js
+++ test/models/User.test.js
@@ -13,4 +13,12 @@ describe("Unit Test for User Class", () =>{
         expect (user.lastUpdated).not.toBeUndefined()
         });
         
+    test('Add getters', () => {
+        const user = new User(1, "carlogilmar", "Carlo", "Bio")
+        expect (user.getUsername).toBe("carlogilmar")
+        expect (user.getBio).toBe("Bio")
+        expect (user.getDateCreated).not.toBeUndefined ()
+        expect (user.getLastUpdated).not.toBeUndefined()
+    });
+            
 });
