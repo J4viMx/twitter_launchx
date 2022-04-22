730444302694c6444683a587b6b266f9ba7e8bb5
JaviMx <javimxmine@hotmail.com>
2022-04-22 14:15:09 -0500

    Sprint 3, requerimiento 2

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -2,7 +2,8 @@ const UseService = require('../services/UserService')
 
 class UserView{
     static createUser(payload){
-        return payload === null && {error: "payload no existe"}  
+        return payload === null ? {error: "payload no existe"}  :
+        payload.usename == null || payload.name == null || payload.id == null ? {error: "necesitan tener un valor válido"} : null
     }
 }
 
